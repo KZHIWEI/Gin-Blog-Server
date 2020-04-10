5633a60b2eb3165ade3593a88ba4fd80934e6bc1
KZHIWEI <zcabhaf@ucl.ac.uk>
2020-04-10 22:18:34 +0800

    improve jwt payload

--- auth.go
+++ auth.go
@@ -5,32 +5,39 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Authenticator(c *gin.Context)(i interface{}, err error){
+func Authenticator(c *gin.Context) (i interface{}, err error) {
 	var loginValues Login
 	if err := c.ShouldBind(&loginValues); err != nil {
 		return "", jwt.ErrMissingLoginValues
 	}
 	username := loginValues.Username
 	password := loginValues.Password
-	email 	 := loginValues.Email
+	email := loginValues.Email
 
 	if (username == "test" && password == "password") || (email == "test@qq.com" && password == "password") {
 		return &UserPayLoad{
-			UserName:  username,
-			id:  "123",
-			Email: "test@qq.com",
+			UserName: username,
+			id:       "123",
+			Email:    "test@qq.com",
 		}, nil
 	}
 
 	return nil, jwt.ErrFailedAuthentication
 }
-
-func AuthMiddleware(key string) (*jwt.GinJWTMiddleware , error) {
+func PayloadFunc(data interface{}) jwt.MapClaims {
+	return jwt.MapClaims {
+		"UserName" : data.(*UserPayLoad).UserName,
+		"id" : data.(*UserPayLoad).id,
+		"Email" : data.(*UserPayLoad).Email,
+	}
+}
+func AuthMiddleware(key string) (*jwt.GinJWTMiddleware, error) {
 	authMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
 		Realm:                 "Blog-server",
 		Key:                   []byte(key),
-		Authenticator: Authenticator,
+		Authenticator:         Authenticator,
 		LoginResponse:         nil,
+		PayloadFunc:           PayloadFunc,
 		LogoutResponse:        nil,
 		RefreshResponse:       nil,
 		IdentityHandler:       nil,
--- server.go
+++ server.go
@@ -7,23 +7,23 @@ import (
 	"os"
 )
 
-func LoadEnv(){
+func LoadEnv() {
 	err := godotenv.Load(".env.yaml")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 		return
 	}
-	GlobalConfig = Config {
-		JwtToken:os.Getenv("JWT-TOKEN"),
-		DbAddress:os.Getenv("DB-Address"),
-		Port:os.Getenv("PORT"),
+	GlobalConfig = Config{
+		JwtToken:  os.Getenv("JWT-TOKEN"),
+		DbAddress: os.Getenv("DB-Address"),
+		Port:      os.Getenv("PORT"),
 	}
 }
 
 type Config struct {
 	JwtToken  string
 	DbAddress string
-	Port 	  string
+	Port      string
 }
 
 var GlobalConfig Config
@@ -31,11 +31,11 @@ var GlobalConfig Config
 func main() {
 	LoadEnv()
 	r := gin.Default()
-	auth,err := AuthMiddleware(GlobalConfig.JwtToken)
-	if err!= nil {
+	auth, err := AuthMiddleware(GlobalConfig.JwtToken)
+	if err != nil {
 		panic(err.Error())
 	}
-	r.POST("/login",auth.LoginHandler)
+	r.POST("/login", auth.LoginHandler)
 	api := r.Group("/api")
 	api.Use(auth.MiddlewareFunc())
 	{
